b07b892148635807af1f9c02e825fe5eb9ae4456
fuck <xtang2828@gmail.com>
2022-04-22 03:07:35 +0800

    refactor

--- trasformation.py
+++ trasformation.py
@@ -5,6 +5,10 @@ Export RKNN model
 
 from rknn.api import RKNN
 
+ONNX_MODEL = './onnx/best.onnx'
+RKNN_MODEL = 'best.rknn'
+DATASET = 'dataset.txt'
+
 if __name__ == '__main__':
     # Create RKNN execution objects
     print("shit")
@@ -13,13 +17,14 @@ if __name__ == '__main__':
 
     # pre-process config
     print('--> config model')
-    rknn.config(mean_values=[[123.675, 116.28, 103.53]], std_values=[[58.395, 58.395, 58.395]],
+    rknn.config(mean_values=[[0, 0, 0]],
+                std_values=[[255, 255, 255]],
                 reorder_channel='0 1 2') # RGB
 
 
     # Load pt model
     print('--> Loading model')
-    ret = rknn.load_pytorch(model='./best.pt', input_size_list=[[3,640,640]])
+    ret = rknn.load_pytorch(model=ONNX_MODEL, input_size_list=[[3,640,640]])
     if ret != 0:
         print('Load pt failed!')
         exit(ret)
@@ -28,7 +33,7 @@ if __name__ == '__main__':
 
     # Build model
     print('--> Building model')
-    ret = rknn.build(do_quantization=True, dataset='./dataset.txt')
+    ret = rknn.build(do_quantization=True, dataset=DATASET)
     if ret != 0:
         print('Build yoloRknn failed!')
         exit(ret)
@@ -36,7 +41,7 @@ if __name__ == '__main__':
 
     # Export rknn model
     print('--> Export RKNN model')
-    ret = rknn.export_rknn('./best.rknn')
+    ret = rknn.export_rknn(RKNN_MODEL)
     if ret != 0:
         print('Export best.rknn failed!')
         exit(ret)
--- util/hd.py
+++ util/hd.py
@@ -3,12 +3,12 @@ import os
 img_root = '../data/traffic/images/'
 
 def generate_dataset_txt(img_root):
-    li = sorted(os.listdir(img_root))
+    li = os.listdir(img_root)
 
     if os.path.exists('../dataset.txt'):
         os.remove('../dataset.txt')
     with open('../dataset.txt', 'a+') as f:
-        for i in range(len(li)):
+        for i in range(len(li[:200])):
             img = li[i]
             img_path = img_root[3:] + img + '\n'
             f.write(img_path)
